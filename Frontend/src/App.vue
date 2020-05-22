5be543f17fa35ab3db3cc036585c68818cb3b6bf
japerry911 <jack.e.perry@outlook.com>
2020-05-22 08:45:54 -0600

    Finished draft of top bar computer menu, responsive side bar is built as well for tablet/phone view

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,14 +1,41 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <app-navigation :navLinks="navLinks" />
+    <router-view></router-view>
   </div>
 </template>
 
+<script>
+import ResponsiveNavigation from './components/ResponsiveNavigation.vue'
+
+export default {
+  components: {
+    appNavigation: ResponsiveNavigation
+  },
+  data () {
+    return {
+      navLinks: [
+        { text: 'Home', path: '/' },
+        { text: 'About', path: '/about' },
+        { text: 'Contact', path: '/contact' },
+        { text: 'Sign In', path: '/sign-in' }
+      ]
+    }
+  }
+}
+</script>
+
 <style lang="scss">
+figure {
+	margin-block-start: 0;
+	margin-block-end: 0;
+	margin-inline-start: 10px;
+	margin-inline-end: 0;
+}
+body {
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ Frontend/src/components/ResponsiveNavigation.vue
@@ -0,0 +1,110 @@
+<template>
+  <nav :style="{ background: background || '#333' }">
+    <ul :style="{ background: background || '#333' }">
+      <figure>
+        <img
+          alt='Skylord Movie Club Logo'
+          src='https://skylord-movie-club.s3.us-east-2.amazonaws.com/Logos/logo_transparent_background.png'
+        />
+      </figure>
+      <div class='list-links'>
+        <li
+          v-for="(link, index) in navLinks"
+          :key="index"
+          @mouseenter="$event.currentTarget.style.background = hoverbackground || '#999'"
+          @mouseleave="$event.currentTarget.style.background = background || '#333'"
+        >
+          <router-link
+            :to="link.path"
+            :style="{ color: linkColor || '#DDD' }"
+          >
+            {{ link.text }}
+          </router-link>
+        </li>
+      </div>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: {
+    navLinks: Array,
+    background: String,
+    linkColor: String,
+    hoverBackground: String
+  }
+}
+</script>
+
+<style scoped lang='scss'>
+nav {
+  height: 70px;
+  width: 100%;
+
+  ul {
+    display: flex;
+    justify-content: space-between;
+    height: 100%;
+    align-items: center;
+    margin-block-start: 0;
+    margin-block-end: 0;
+    padding-inline-start: 0;
+
+    figure {
+      width: 10%;
+      height: 100%;
+
+      img {
+        width: 100%;
+        height: auto;
+      }
+    }
+
+    .list-links {
+      display: flex;
+      width: 50%;
+      height: 100%;
+
+      li {
+        list-style-type: none;
+        height: 100%;
+        width: 100%;
+
+        a {
+          height: 100%;
+          width: 100%;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          text-decoration: none;
+        }
+      }
+    }
+  }
+}
+
+@media screen and (max-width: 759px) {
+  nav {
+    ul {
+      position: absolute;
+      width: 300px;
+      flex-direction: column;
+
+      figure {
+        width: 100%;
+        height: auto;
+      }
+
+      .list-links {
+        flex-direction: column;
+        width: 100%;
+
+        li {
+          width: 100%;
+        }
+      }
+    }
+  }
+}
+</style>
