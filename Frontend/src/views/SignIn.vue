c638af661a176c980468cbc432fbaebb5ca26bc2
japerry911 <jack.e.perry@outlook.com>
2020-05-23 11:58:15 -0600

    Layout finished, ready for functionality

--- Frontend/src/views/SignIn.vue
+++ Frontend/src/views/SignIn.vue
@@ -10,6 +10,12 @@
                     <h3 class='form-title'>
                         User Sign In Portal
                     </h3>
+                    <figure>
+                        <img
+                            alt='Skylord Movie Club Logo'
+                            src='https://skylord-movie-club.s3.us-east-2.amazonaws.com/Logos/dark_logo_transparent_background.png'
+                        />
+                    </figure>
                     <p>Sign in or Create a New Account</p>
                 </div>
                 <div class='form-input'>
@@ -27,8 +33,8 @@
                     >
                 </div>
                 <div class='btns-div'>
-                    <button class='sign-in-btn'>Sign In</button>
-                    <button class='create-btn'>Sign Up</button>
+                    <button>Sign Up</button>
+                    <button>Sign In</button>
                 </div>
             </form>
         </div>
@@ -56,17 +62,19 @@ div.main-sign-in-div {
         display: flex;
         justify-content: center;
         align-items: center;
+        background-color: $accentLightGray;
 
         form {
             width: 75%;
             height: 90%;
             margin: 5rem;
-            background-color: lightgray;
+            background-color: $accentLightOrange;
             border-radius: 12pt;
             display: flex;
             flex-direction: column;
             justify-content: space-around;
             align-items: center;
+            border: 1pt solid $primaryOrange;
 
             .greeting-div {
                 display: flex;
@@ -77,11 +85,21 @@ div.main-sign-in-div {
 
                 .form-title {
                     font-size: 2.5rem;
-                    color: #F67743;
+                    color: #000;
                 }
 
                 p {
                     font-size: 1.25rem;
+                }
+
+                figure {
+                    width: 15%;
+                    height: auto;
+
+                    img {
+                        width: 100%;
+                        height: auto;
+                    }
                 }
             }
 
@@ -101,6 +119,30 @@ div.main-sign-in-div {
                     height: 50%;
                     padding-left: 1rem;
                     font-size: 1rem;
+                }
+            }
+
+            .btns-div {
+                width: 75%;
+                display: flex;
+                justify-content: space-evenly;
+
+                button {
+                    width: 14rem;
+                    height: 5rem;
+                    font-size: 1.5rem;
+                    background-color: $primaryOrange;
+                    border: 1pt solid #000;
+                    border-radius: 12pt;
+                    color: #000;
+                    cursor: pointer;
+                    transition: 250ms ease-in;
+                }
+
+                button:hover {
+                    background-color: $accentLightGray;
+                    border: 1pt solid $primaryOrange;
+                    color: $primaryOrange;
                 }
             }
         }
