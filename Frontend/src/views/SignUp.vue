1bf307512b75a89c278b432bc807414471307570
japerry911 <jack.e.perry@outlook.com>
2020-05-25 10:10:45 -0600

    Frontend layout draft complete

--- Frontend/src/views/SignUp.vue
+++ Frontend/src/views/SignUp.vue
@@ -7,6 +7,44 @@
         />
         <div class='form-div'>
             <form>
+                <div class='greeting-div'>
+                    <h3 class='form-title'>
+                        Account Sign Up
+                    </h3>
+                    <figure>
+                        <img
+                            alt='Skylord Movie Club Logo'
+                            src='https://skylord-movie-club.s3.us-east-2.amazonaws.com/Logos/dark_logo_transparent_background.png'
+                        />
+                    </figure>
+                </div>
+                 <div class='form-input'>
+                    <label for='username'>Username</label>
+                    <input
+                        type='text'
+                        id='username'
+                        v-model='username'
+                    >
+                </div>
+                <div class='form-input'>
+                    <label for='password'>Password</label>
+                    <input
+                        type='password'
+                        id='password'
+                        v-model='password'
+                    >
+                </div>
+                <div class='form-input'>
+                    <label for='confirm-password'>Confirm Password</label>
+                    <input
+                        type='password'
+                        id='confirm-password'
+                        v-model='confirmPassword'
+                    >
+                </div>
+                <div class='btn-div'>
+                    <button :disabled='!validate'>Create Account</button>
+                </div>
             </form>
         </div>
     </div>
@@ -18,6 +56,18 @@ import HeroHeader from '../components/HeroHeader.vue'
 export default {
     components: {
         heroHeader: HeroHeader
+    },
+    data () {
+        return {
+            username: '',
+            password: '',
+            confirmPassword: ''
+        }
+    },
+    computed: {
+        validate () {
+            return this.username !== '' && this.password !== '' && this.password === this.confirmPassword
+        }
     }
 }
 </script>
@@ -46,6 +96,76 @@ div.main-div-sign-up {
             justify-content: space-around;
             align-items: center;
             border: 1pt solid $primaryOrange;
+
+            .greeting-div {
+                display: flex;
+                height: 10%;
+                flex-direction: column;
+                justify-content: flex-start;
+                align-items: center;
+
+                .form-title {
+                    font-size: 2.5rem;
+                    color: #000;
+                }
+
+                figure {
+                    width: 15%;
+                    height: auto;
+
+                    img {
+                        width: 100%;
+                        height: auto;
+                    }
+                }
+            }
+
+            .form-input {
+                display: flex;
+                flex-direction: column;
+                height: 10%;
+                width: 50%;
+
+                label {
+                    font-size: 1.15rem;
+                    font-weight: bold;
+                }
+
+                input {
+                    border-radius: 12pt;
+                    height: 50%;
+                    padding-left: 1rem;
+                    font-size: 1rem;
+                }
+            }
+
+            .btn-div {
+                width: 75%;
+                display: flex;
+                justify-content: space-evenly;
+
+                button {
+                    width: 14rem;
+                    height: 5rem;
+                    font-size: 1.5rem;
+                    background-color: $primaryOrange;
+                    border: 1pt solid #000;
+                    border-radius: 12pt;
+                    color: #000;
+                    cursor: pointer;
+                    transition: 250ms ease-in;
+                }
+
+                button:hover:enabled {
+                    background-color: $accentLightGray;
+                    border: 1pt solid $primaryOrange;
+                    color: $primaryOrange;
+                }
+
+                button:hover:disabled {
+                    background-color: red;
+                }
+            }
         }
     }
 }
