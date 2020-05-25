0976b957c393fe68c1c8198b0cee588043b119ec
japerry911 <jack.e.perry@outlook.com>
2020-05-25 10:16:36 -0600

    Backend create user route added

--- Backend/app/controllers/users_controller.rb
+++ Backend/app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+    def create
+        @user = User.create(
+            username: user_params[:username],
+            password: user_params[:password],
+            password_confirmation: user_params[:password]
+        )
+
+        if @user
+            render json: { status: :created }
+        else
+            render json: { status: 500 }
+        end
+    end
+
+    private
+
+        def user_params
+            params.require(:user).permit(:username, :password)
+        end
 end
