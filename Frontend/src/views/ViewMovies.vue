e14bf052a74a0acc1afc5b3f0c486fb5b92778b1
japerry911 <jack.e.perry@outlook.com>
2020-05-27 10:19:11 -0600

    added loading screen and hero header to viewMovies view

--- Frontend/src/views/ViewMovies.vue
+++ Frontend/src/views/ViewMovies.vue
@@ -1,14 +1,39 @@
 <template>
-    <h1>Movies</h1>
+    <div class='view-movies-main-div' v-if='!loadingStatus'>
+        <hero-header
+            headerImageUrl='https://skylord-movie-club.s3.us-east-2.amazonaws.com/ViewMovies/jonathan-daniels-CdVG9f96kyg-unsplash.jpg'
+            headerText='View Movies'
+        />
+        <h1>Movies Screen</h1>
+    </div>
+    <div class='loading-div' v-else>
+        <vue-spinner />
+    </div>
 </template>
 
 <script>
+import HeroHeader from '../components/HeroHeader.vue'
+import Spinner from 'vue-simple-spinner'
+
 export default {
+    components: {
+        heroHeader: HeroHeader,
+        vueSpinner: Spinner
+    },
     mounted () {
         this.$store.dispatch('getMovies')
+    },
+    computed: {
+        loadingStatus () {
+            return this.$store.getters.loadingStatus
+        }
     }
 }
 </script>
 
 <style scoped lang='scss'>
+div.view-movies-main-div {
+    width: 100%;
+    height: 1000px;
+}
 </style>
