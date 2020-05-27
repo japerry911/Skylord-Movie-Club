39e5944745b4308e7430d7f2c89097c8c16116d3
japerry911 <jack.e.perry@outlook.com>
2020-05-27 10:08:41 -0600

    Backend Movie Index route created

--- Backend/app/controllers/movies_controller.rb
+++ Backend/app/controllers/movies_controller.rb
@@ -1,2 +1,7 @@
 class MoviesController < ApplicationController
+    def index
+        @movies = Movie.all
+
+        render json: { movies: @movies }, include: { reviews: { only: :rating }}
+    end
 end
--- Backend/config/routes.rb
+++ Backend/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:create]
+  resources :movies, only: [:index]
   post '/login', to: 'sessions#login'
   get 'most-recent-reviews', to: 'reviews#most_recent_reviews'
 end
