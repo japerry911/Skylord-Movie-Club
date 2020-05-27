93101c5d8605ae06213267ece6fdfbf9797561f6
japerry911 <jack.e.perry@outlook.com>
2020-05-27 14:07:37 -0600

    Working Vuex action for showMovie

--- Frontend/src/views/ShowMovie.vue
+++ Frontend/src/views/ShowMovie.vue
@@ -1,11 +1,43 @@
 <template>
-    <h1>Show Movie: {{ this.$route.params.id }}</h1>
+    <div class='show-movie-main-div' v-if='!loadingStatus'>
+        <hero-header
+            headerText='Some Movie'
+            headerImageUrl='https://skylord-movie-club.s3.us-east-2.amazonaws.com/ShowMovie/peter-lewicki-Wfh650C1OHU-unsplash.jpg'
+        />
+        <h1>Show Movie: {{ movie.id }}</h1>
+    </div>
+    <div class='loading-div' v-else>
+        <vue-spinner />
+    </div>
 </template>
 
 <script>
+import HeroHeader from '../components/HeroHeader.vue'
+import Spinner from 'vue-simple-spinner'
+
 export default {
+    components: {
+        heroHeader: HeroHeader,
+        vueSpinner: Spinner
+    },
+    mounted () {
+        this.$store.dispatch('getMovie', this.$route.params.id)
+    },
+    computed: {
+        movie () {
+            return this.$store.getters.showMovie
+        },
+        loadingStatus () {
+            return this.$store.getters.loadingStatus
+        }
+    }
 }
 </script>
 
 <style scoped lang='scss'>
+div.show-movie-main-div {
+    min-height: 1000px;
+    width: 100%;
+    background-color: $accentLightGray;
+}
 </style>
