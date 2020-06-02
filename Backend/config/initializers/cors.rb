fdaea53e25e3e1881f9cfdf53ce971de74d15199
japerry911 <jack.e.perry@outlook.com>
2020-06-02 11:31:14 -0600

    Deployed

--- Backend/config/initializers/cors.rb
+++ Backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins 'http://localhost:8080'
+        origins 'http://skylord-movie-club.herokuapp.com', 'https://skylord-movie-club.herokuapp.com', 'http://localhost:8080'
 
         resource '*',
         headers: :any,
